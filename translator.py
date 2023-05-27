0b50cfbd2a837e4fdb5ef00ffbb925ef5e32872b
akshay-ken <akshaybest3120@gmail.com>
2023-05-28 00:13:12 +0530

    added new line of code to files and stage one completed

--- translator.py
+++ translator.py
@@ -1,3 +1,11 @@
 print('Type "en" if you want to translate from French into English, or "fr" if you '
       'want to translate from English into French:')
 
+language_selected = input()  # taking target language from user
+
+print('Type the word you want to translate:')
+
+word_to_translate = input()
+
+print(f'You chose {language_selected} as a language to translate {word_to_translate}.')
+
