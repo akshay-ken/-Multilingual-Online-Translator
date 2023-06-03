31ba2e70368469f3c9900358723dc46082ee4c30
akshay-ken <akshaybest3120@gmail.com>
2023-06-03 21:26:31 +0530

    adding code to browse website [2nd stage 1st commit]

--- translator.py
+++ translator.py
@@ -1,11 +1,41 @@
+import requests  # importing requests to make HTTP request
+from bs4 import BeautifulSoup  # importing it to work with html tags
+
 print('Type "en" if you want to translate from French into English, or "fr" if you '
       'want to translate from English into French:')
 
+headers = {'User-Agent': 'Mozilla/5.0'}  # Presenting itself as browser
+
 language_selected = input()  # taking target language from user
+
+from_language = ''
+to_language = ''
+if language_selected == 'fr':
+      from_language = 'french'
+      to_language = 'english'
+
+elif language_selected == 'en':
+      from_language = 'english'
+      to_language = 'french'
+
 
 print('Type the word you want to translate:')
 
-word_to_translate = input()
+word_to_translate = input()  # taking word to translate
 
 print(f'You chose {language_selected} as a language to translate {word_to_translate}.')
+
+# making url dynamically by taking user input as arguments to make complete url
+
+url = f'https://context.reverso.net/translation/{from_language}-{to_language}/{word_to_translate}'
+
+# sending HTTP request as get method with target url and header details
+page = requests.get(url, headers=headers)
+
+
+
+
+
+
+
 
